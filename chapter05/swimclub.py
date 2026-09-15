49619c1676b1cf55ba5c9f6e7a53c3be020f7ed4
asaerxd6767 <mohammedasser070@gmail.com>
2026-09-15 18:20:35 +0300

    Add swim data processing functionality and HTML visualization for chapter 05

---
+++ chapter05/swimclub.py
@@ -0,0 +1,27 @@
+import statistics
+FOLDER = 'swimdata/'
+
+def read_swim_data(filename: str):
+    swimmer, age, distance, stroke = filename.removesuffix('.txt').split('-')
+    with open(FOLDER + filename) as f:
+        lines = f.readlines()
+        times = lines[0].strip().split(',')
+        
+    converts = []
+    for time in times:
+        if ':' in time:
+            minutes, rest = time.split(':')
+            seconds, hundreds = rest.split('.')
+            
+        else:
+            minutes = 0
+            seconds, hundreds = time.split('.')
+        converted_time = (int(minutes) * 100 * 60) + (int(seconds) * 100) + int(hundreds)
+        converts.append(converted_time)
+
+    average = statistics.mean(converts)
+    mins_secs, hundredths = str(round(average / 100, 2)).split('.')
+    minutes = int(mins_secs) // 60
+    seconds = int(mins_secs) - minutes * 60
+    average = f'{minutes}:{seconds}.{hundredths}'
+    return swimmer, age, distance, stroke, times, average, converts
